df6404f0ac583401391b2e14a058caaa666d46b5
agiledineshreddy <126139844+agiledineshreddy@users.noreply.github.com>
2024-08-06 11:29:08 +0530

    s

--- about.js
+++ about.js
@@ -2,3 +2,9 @@
 document.addEventListener('DOMContentLoaded', function() {
     // Code to run when the document is ready
 });
+const x=document.querySelector('.btn-b');
+const y=document.querySelector('.nav-items2');
+x.addEventListener('click',function(){
+    x.classList.toggle('isactive');
+    y.classList.toggle('isactive');
+});
--- projects.js
+++ projects.js
@@ -2,3 +2,9 @@ function toggleDetail(projectId) {
     var element=document.getElementById(projectId);
     element.classList.toggle('hid');
 }
+const x=document.querySelector('.btn-b');
+const y=document.querySelector('.nav-items2');
+x.addEventListener('click',function(){
+    x.classList.toggle('isactive');
+    y.classList.toggle('isactive');
+});
